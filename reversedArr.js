03ffc48f19904431635b48bfdab924f635bc97b7
Harshini <harshinibariki@gmail.com>
2017-05-03 09:15:02 -0700

    feat:reverse of an array

---
+++ reversedArr.js
@@ -0,0 +1,9 @@
+// print reverse of an array
+var arr = [1,2,3,4,5];
+var i= arr.length, j = 0, reversedArr = [];
+while(i > 0){
+    revreseArr[j] = arr[i-1];
+    i--;
+    j++;
+}
+console.log(reversedArr);
