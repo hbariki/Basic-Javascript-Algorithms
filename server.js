da17e352c9b1ff194003bee9c6344f86b868b326
Harshini <harshinibariki@gmail.com>
2017-05-03 09:09:11 -0700

    Refactor: print nth fibonacci number

--- server.js
+++ server.js
@@ -1,6 +1,1 @@
 // check whether it is a palindrome
-isPalindrome('level');
-function isPalindrome(str){
-    str = str.replace(' ', ' ').toLowerCase();
-    return (str == str.split('').reverse('').join(''))
-}
