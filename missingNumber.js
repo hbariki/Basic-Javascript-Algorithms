247b9fa47ea31af90ea20de8fbbce632f51d01b6
Harshini <harshinibariki@gmail.com>
2017-05-03 13:36:36 -0700

    fix : Add correct syntax

--- missingNumber.js
+++ missingNumber.js
@@ -1,5 +1,6 @@
+// find the missing number and works for only 1 difference
 var arr = [5,4,2,1];
-var n = arr1.length;
+var n = arr.length;
 total = (n+1)*(n+2)/2;
 for(var i=0; i<arr.length;i++){
     total -= arr[i];
