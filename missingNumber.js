f8b31812653cc896cd893106e99619e615d5ae4d
Harshini <harshinibariki@gmail.com>
2017-05-03 20:10:13 -0700

    feat: missing a number in progressive array

--- missingNumber.js
+++ missingNumber.js
@@ -1,8 +1,10 @@
 // find the missing number and works for only 1 difference
-var arr = [5,4,2,1];
-var n = arr.length;
-total = (n+1)*(n+2)/2;
-for(var i=0; i<arr.length;i++){
-    total -= arr[i];
+var arr = [3, 6, 9, 15, 18, 21];
+var diff = (arr[arr.length-1] - arr[0])/arr.length;
+var i = 0;
+while(i < arr.length - 1) {
+    if(arr[i+1] - arr[i] !== diff) {
+        console.log(arr[i] + diff);
+    }
+    i++;
 }
-console.log(total);
