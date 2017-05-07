0ec6c9d818c23160635a8b1599ca74f366e33bb2
Harshini <harshinibariki@gmail.com>
2017-05-07 09:13:57 -0700

    feat:mergeSortedArray

--- swapwithouttemp.js
+++
@@ -1,9 +0,0 @@
-function swapNumb(a, b){
-    console.log('before swap: ','a: ', a, 'b: ', b);
-    b = b -a;
-    a = a+ b;
-    b = a-b;
-    console.log('after swap: ','a: ', a, 'b: ', b);
-}
-
- swapNumb(2, 3);
